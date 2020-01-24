27afb54c2d66cdbd7b533f95747f032967a7b6b6
Duy <huynhduytam2210@gmail.com>
2020-01-24 10:15:14 +0700

    update payload jwt

--- src/bussiness/module/auth/jwt.strategy.ts
+++ src/bussiness/module/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { JWTConstants } from 'src/common/config/jwt-constants';
+import UserView from '../user/view/user.view';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
     }
 
-    async validate(payload: any) {
-        return payload;
+    async validate(payload: any): Promise<UserView>{
+        return payload.user;
     }
 }
